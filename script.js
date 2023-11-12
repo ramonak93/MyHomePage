a9480db6610e86ec73e4b63b72607f3a3055479a
ramonak <serjiond@gmail.com>
2023-11-12 19:22:29 +0200

    added alert script to "Submit" (contact me page)

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let btn = document.querySelector('#submit');
+    btn.addEventListener('click', function() {
+        alert("This button is not working yet! Sorry for the inconvinience.");
+    })
+})
